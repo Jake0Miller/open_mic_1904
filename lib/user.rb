01e0fd20edb012e06639cc6ddb4ef0a42e02d616
Jake Miller <jake0miller@gmail.com>
2019-05-08 09:17:16 -0600

    Create user.rb

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+  attr_reader :name, :jokes
+
+  def initialize(name)
+    @name = name
+    @jokes = []
+  end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(user, joke)
+    user.learn(joke)
+  end
+end
