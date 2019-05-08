47c2b396b67cbfcad2566a5ecbd0fd1e4bd226bb
Jake Miller <jake0miller@gmail.com>
2019-05-08 09:24:25 -0600

    Create open_mic_test.rb

---
+++ test/open_mic_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+require './lib/user'
+require './lib/open_mic'
+require 'pry'
+
+class OpenMicTest < MiniTest::Test
+  def setup
+  end
+end
