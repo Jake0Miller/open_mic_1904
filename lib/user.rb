4c4896a37b592750ebedd9f0f72d6e5642df1950
Jake Miller <jake0miller@gmail.com>
2019-05-08 09:23:06 -0600

    Refactor @jokes from array to hash

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,24 @@
 class User
-  attr_reader :name, :jokes
+  attr_reader :name
 
   def initialize(name)
     @name = name
-    @jokes = []
+    @jokes = {}
+  end
+
+  def jokes
+    @jokes.values
   end
 
   def learn(joke)
-    @jokes << joke
+    @jokes[joke.id] = joke
   end
 
   def tell(user, joke)
     user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    @jokes[id]
   end
 end
